df6a838df904c41a0940713bef23d8069c2d773f
Justin <justinxclusive@gmail.com>
2019-07-21 18:16:13 +0800

    initial settings

---
+++ src/DirectReferralServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Jxclsv\Referable\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class DirectReferralServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
