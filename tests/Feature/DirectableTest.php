75c25b667ba2cccce67bf916386fb2e1b3cec109
Justin <justinxclusive@gmail.com>
2019-08-14 02:56:50 +0800

    added feature test

---
+++ tests/Feature/DirectableTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Jxclsv\Referable\Tests\Feature;
+
+use Jxclsv\Referable\Models\DirectReferralWallet;
+use Jxclsv\Referable\Models\User;
+use Jxclsv\Referable\Tests\TestCase;
+
+class DirectableTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function it_can_have_directs_and_add_balance_with_bonus()
+    {
+        $this->user->createDirectReferralWallet();
+
+        $this->user->addDirects(
+            $this->factory->create(User::class, ['sponsor_id' => $this->user->id])
+        );
+
+        $this->user->addDirects(
+            $this->factory->create(User::class, ['sponsor_id' => $this->user->id])
+        );
+
+        $this->assertCount(2, $this->user->directReferralBonuses);
+        $this->assertEquals(200, $this->user->directReferralWallet->fresh()->balance);
+    }
+}
